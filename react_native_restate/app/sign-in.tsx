ba57e0e1a93415b5fa7c7ec04e239005d4800003
manuelMarkDenver <manuel.markdenver@gmail.com>
2025-07-12 03:30:45 +0800

    react native initial setup

---
+++ react_native_restate/app/sign-in.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+
+const SignIn = () => {
+  const handleLogin = () => {
+    // Handle login logic here
+    console.log("Login button pressed");
+  };
+
+  return (
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+
+        <View className="px-10">
+          <Text className="text-base text-center uppercase font-rubik text-black-200">
+            Welcome To Mapiles Realty
+          </Text>
+
+          <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
+            Let's Get You Closer to {"\n"}
+            <Text className="text-lg font-rubik text-black-200 mt-12">
+              Your Ideal Home
+            </Text>
+          </Text>
+
+          <TouchableOpacity
+            onPress={handleLogin}
+            className="bg-white shadow-lg shadow-zinc-400 rounded-full w-full py-4 mt-5"
+          >
+            <View className="flex flex-row items-center justify-center">
+              <Image
+                source={icons.google}
+                className="w-5 h-5"
+                resizeMode="contain"
+              />
+              <Text className="text-lg font-rubik-medium text-black-300 ml-2">
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
