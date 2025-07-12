15095818990418b98d23d70fc81ddce6a88a78da
manuelMarkDenver <manuel.markdenver@gmail.com>
2025-07-12 12:20:17 +0800

    removed the $ sign

--- react_native_restate/components/Cards.tsx
+++ react_native_restate/components/Cards.tsx
@@ -87,7 +87,7 @@ export const Card = ({ item, onPress }: Props) => {
 
         <View className="flex flex-row items-center justify-between mt-2">
           <Text className="text-base font-rubik-bold text-primary-300">
-            ${item.price}
+            {item.price}
           </Text>
           <Image
             source={icons.heart}
