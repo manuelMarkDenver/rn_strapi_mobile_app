dcc5786f7a8fcee882d9989214c0212bd34a2b45
manuelMarkDenver <manuel.markdenver@gmail.com>
2025-07-12 10:33:48 +0800

    working home and profile pages

--- react_native_restate/app/(root)/(tabs)/explore.tsx
+++ react_native_restate/app/(root)/(tabs)/explore.tsx
@@ -1,12 +1,53 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { View, Text, Image, TouchableOpacity, FlatList } from "react-native";
+import React from "react";
+import { Link, router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const Explore = () => {
+import images from "@/constants/images";
+import icons from "@/constants/icons";
+import Search from "@/components/Search";
+import { Card, FeaturedCard } from "@/components/Cards";
+import Filters from "@/components/Filters";
+
+const index = () => {
   return (
-    <View>
-      <Text>Explore</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-white h-full">
+      <FlatList
+        data={[1, 2, 3, 4]}
+        renderItem={() => <Card />}
+        keyExtractor={(item) => item.toString()}
+        numColumns={2}
+        contentContainerClassName="pb-32"
+        columnWrapperClassName="flex gap-5 px-5"
+        showsHorizontalScrollIndicator={false}
+        ListHeaderComponent={
+          <View className="px-5">
+            <View className="flex flex-row items-center justify-between mt-5">
+              <TouchableOpacity
+                onPress={() => router.back()}
+                className="flex flex-row bg-primary-200 rounded-full size-11 items-center justify-center"
+              >
+                <Image source={icons.backArrow} className="size-5" />
+              </TouchableOpacity>
 
-export default Explore
+              <Text className="text-base mr-2 text-center font-rubik-medium text-black-300">
+                Search for Your Ideal Home
+              </Text>
+              <Image source={icons.bell} className="w-6 h-6" />
+            </View>
+
+            <Search />
+
+            <View className="mt-5">
+              <Text className="text-xl font-rubik-bold text-blue-300 mt-5">
+                Found 5 properties
+              </Text>
+            </View>
+          </View>
+        }
+      />
+    </SafeAreaView>
+  );
+};
+
+export default index;
